0aac7e8d8baebcdb7fc30e04a3eb434315b71b48
bccoaaof <bccoaaof@gmail.com>
2021-11-30 14:26:18 +0800

    update 0608

---
+++ 0608_calculator/src/main/java/tw/sgft/m0608/M0608.java
@@ -0,0 +1,20 @@
+package tw.sgft.m0608;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class M0608 extends AppCompatActivity {
+
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.m0608);
+
+            setupViewComponent();
+      }
+
+      private void setupViewComponent() {
+
+      }
+}
