db0110edee1519ced77db55c0fb3a704e660e808
bccoaaof <bccoaaof@gmail.com>
2021-12-02 13:28:02 +0800

    update 2021 12 02 1330

--- 0609_IndexMicro/src/main/java/tw/tcnr09/m0609/Menu03.java
+++ 0609_IndexMicro/src/main/java/tw/tcnr09/m0609/Menu03.java
@@ -1,8 +1,5 @@
 package tw.tcnr09.m0609;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -15,11 +12,14 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Menu01 extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-      Intent intent01 = new Intent();
+public class Menu03 extends AppCompatActivity {
+
+      Intent intent03 = new Intent();
       private TextView myname;
       private TextView tv;
+      private String mode_title;
 
       @Override
       protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +32,6 @@ public class Menu01 extends AppCompatActivity {
       private void setupViewComponent() {
             LinearLayout mlay02 = (LinearLayout) findViewById(R.id.lay02);
             myname = (TextView) findViewById(R.id.myname);
-
-
 
             //-----------------------------------------------------
 
@@ -67,10 +65,16 @@ public class Menu01 extends AppCompatActivity {
             // ----------------------------------------------------
             TextView objt001 = (TextView) findViewById(R.id.objT001); // 取出參考物件
             objt001.setVisibility(View.GONE); // 有三種：顯示，不顯示，消失。設定參考物件消失(GONE)
+
+            // 設定class標題
+            intent03 = this.getIntent();
+            mode_title = intent03.getStringExtra("subname");
+//            myname.setText(mode_title);
+            this.setTitle(this.getResources().getIdentifier(mode_title, "string", getPackageName()));
             // ----------------------------------------------------
 
             try {
-                  for (int i = 5; i <= 20; i++) {// 設定走20圈, 遇到沒資料自動停止.
+                  for (int i = 1; i <= 30; i++) {// 設定走20圈, 遇到沒資料自動停止.
                         tv = new TextView(this); // tv 繼承TextView
                         tv.setId(i); // 寫入配置碼ID 代號
                         // %02d執行十進制整數轉換d，格式化補零，寬度為2。 因此，一個int參數，它的值是7
@@ -78,7 +82,7 @@ public class Menu01 extends AppCompatActivity {
                         String microNo = String.format("%02d", i);
 //                Log.d(TAG,             microNo                );
                         // 取得string 裏頭相對應的ID 碼
-                        int id = getResources().getIdentifier("m" + microNo, "string", getPackageName());
+                        int id = getResources().getIdentifier(mode_title + microNo, "string", getPackageName());
                         // --------------------------------------------
                         if (id == 0) {
                               break; // 假如 getIdentifier 找不到滿足資料, 會傳回 0 , 所以中斷迴圈
@@ -104,19 +108,20 @@ public class Menu01 extends AppCompatActivity {
             }
 
 
+
       }
 
       //
       private View.OnClickListener clkOn = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                  int ii = (v.getId()); // 下層巨集前兩碼
-                  String mm = String.format("%02d", ii);
-                  String subname = "m" + mm;
+//                  int ii = (v.getId()); // 下層巨集前兩碼
+//                  String mm = String.format("%02d", ii);
+//                  String subname = "m" + mm;
 
-                  intent01.putExtra("subname", subname);
-                  intent01.setClass(Menu01.this, Menu02.class);
-                  startActivity(intent01);
+//                  intent03.putExtra("subname", subname);
+//                  intent03.setClass(Menu03.this, Menu03.class);
+//                  startActivity(intent03);
             }
       };
 
@@ -129,7 +134,7 @@ public class Menu01 extends AppCompatActivity {
 
       @Override
       public void onBackPressed() {
-            Toast.makeText(getApplicationContext(),"禁用返回建!!",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "禁用返回建!!", Toast.LENGTH_LONG).show();
 //            super.onBackPressed();
       }
 
